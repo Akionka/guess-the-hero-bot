b731b8d54c87988f9dbb7c37155d0520535c5db2
Akionka <42269025+Akionka@users.noreply.github.com>
2025-03-13 01:40:11 +0300

    Move string binary encoding to another file

--- internal/data/item.go
+++ internal/data/item.go
@@ -6,7 +6,7 @@ type Item struct {
 	ID          int    `db:"item_id"`
 	DisplayName string `db:"display_name"`
 	ShortName   string `db:"short_name"`
-	Image       Image  `db:"-"` // Might be nil if item somehow does not have an image
+	Image       Image  `db:"-"`
 }
 
 func (i Item) LogValue() slog.Value {
