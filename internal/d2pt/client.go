d46d78fb902307f3afbfeb4331bde67337ec8b0b
Akionka <42269025+Akionka@users.noreply.github.com>
2025-03-13 01:38:52 +0300

    Refactor question fetching from D2PT API logic

--- internal/d2pt/client.go
+++ internal/d2pt/client.go
@@ -1,11 +1,9 @@
 package d2pt
 
 import (
-	"context"
 	"encoding/json"
 	"errors"
 	"net/http"
-	"net/url"
 )
 
 type Client struct {
@@ -18,71 +16,8 @@ func NewClient(httpClient *http.Client) *Client {
 	}
 }
 
-func (c *Client) FetchQuestion(ctx context.Context, isWon bool) (*Question, error) {
-	qr := new(Question)
-	isWonStr := "0"
-	if isWon {
-		isWonStr = "1"
-	}
-	requestURL := url.URL{
-		Scheme: "https",
-		Host:   "dota2protracker.com",
-		Path:   "api/gth/random-question",
-		RawQuery: url.Values{
-			"won":         []string{isWonStr},
-			"p_min_score": []string{"3"},
-			"TCACHE":      []string{"0"},
-		}.Encode(),
-	}
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, requestURL.String(), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Add("Referer", "https://dota2protracker.com/")
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, errors.New("unexpected status code: " + resp.Status)
-	}
-
-	if err = json.NewDecoder(resp.Body).Decode(qr); err != nil {
-		return nil, err
-	}
-	return qr, nil
-}
-
-// Question represents a question fetched from the Dota2ProTracker API.
-type Question struct {
-	ID              int     `json:"id"`
-	MatchID         int64   `json:"match_id"`
-	HeroID          int     `json:"hero_id"`
-	AccountID       int64   `json:"account_id"`
-	IsPro           IntBool `json:"is_pro"`
-	Name            string  `json:"name"`
-	Pos             string  `json:"pos"`
-	MMR             int     `json:"mmr"`
-	ActivateTime    int64   `json:"activate_time"`
-	Score1          string  `json:"score1"`
-	Score2          string  `json:"score2"`
-	Items           []int   `json:"items"`
-	ItemsWithScores []struct {
-		P      float64 `json:"p"`
-		Score1 float64 `json:"score1"`
-		Score2 int     `json:"score2"`
-		ItemID int     `json:"item_id"`
-	}
-	IsWon IntBool `json:"won"`
-	// CreatedAt    *time.Time `json:"created_at"`
-	WrongOptions []int `json:"wrong_options"`
-}
-
 // IntBool is a custom boolean type that unmarshals from JSON integers (0 or 1).
+// It is used to represent boolean values in the Dota2ProTracker API.
 type IntBool bool
 
 func (ib *IntBool) UnmarshalJSON(data []byte) error {
@@ -97,6 +32,7 @@ func (ib *IntBool) UnmarshalJSON(data []byte) error {
 	default:
 		return errors.New("invalid value for IntBool")
 	}
+
 	return nil
 }
 
---
+++ internal/d2pt/question.go
@@ -0,0 +1,74 @@
+package d2pt
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/url"
+)
+
+func (c *Client) FetchQuestion(ctx context.Context, isWon bool) (*QuestionResponse, error) {
+	qr := new(QuestionResponse)
+	isWonStr := "0"
+	if isWon {
+		isWonStr = "1"
+	}
+	requestURL := url.URL{
+		Scheme: "https",
+		Host:   "dota2protracker.com",
+		Path:   "api/gth/random-question",
+		RawQuery: url.Values{
+			"won":         []string{isWonStr},
+			"p_min_score": []string{"3"},
+			"TCACHE":      []string{"0"},
+		}.Encode(),
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, requestURL.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Referer", "https://dota2protracker.com/")
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("unexpected status code: " + resp.Status)
+	}
+
+	if err = json.NewDecoder(resp.Body).Decode(qr); err != nil {
+		return nil, err
+	}
+
+	return qr, nil
+}
+
+// QuestionResponse represents a question fetched from the Dota2ProTracker API.
+type QuestionResponse struct {
+	ID              int     `json:"id"`
+	MatchID         int64   `json:"match_id"`
+	HeroID          int     `json:"hero_id"`
+	AccountID       int64   `json:"account_id"`
+	IsPro           IntBool `json:"is_pro"`
+	Name            string  `json:"name"`
+	Pos             string  `json:"pos"`
+	MMR             int     `json:"mmr"`
+	ActivateTime    int64   `json:"activate_time"`
+	Score1          string  `json:"score1"`
+	Score2          string  `json:"score2"`
+	Items           []int   `json:"items"`
+	ItemsWithScores []struct {
+		P      float64 `json:"p"`
+		Score1 float64 `json:"score1"`
+		Score2 int     `json:"score2"`
+		ItemID int     `json:"item_id"`
+	}
+	IsWon IntBool `json:"won"`
+	// CreatedAt    *time.Time `json:"created_at"`
+	WrongOptions []int `json:"wrong_options"`
+}
